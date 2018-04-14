ad6b4cd7449a7452bce1c25c6438f720627663da
wubc1985 <wbc121whl@163.com>
2018-04-14 10:22:24 +0800

    seconde

--- src/main/java/com/tg/bootdemo/controller/TestController.java
+++ src/main/java/com/tg/bootdemo/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.tg.bootdemo.controller;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,5 +11,8 @@ public class TestController {
 	@RequestMapping("/test")
 	public String test() {
 		return "test";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(TestController.class, args);
 	}
 }
